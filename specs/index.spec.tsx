a427370fcedadf65045f9699c67fa564195f4002
Kagami <moe.pascal@gmail.com>
2018-03-27 20:35:44 +0800

    add more tests

--- specs/index.spec.tsx
+++ specs/index.spec.tsx
@@ -1,12 +1,13 @@
 jest.mock('@fortawesome/react-fontawesome')
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { mount } from 'enzyme'
+import { last } from 'lodash'
 import React from 'react'
 import Icon, { isV4 } from '../src'
 
 (FontAwesomeIcon as jest.Mock).mockImplementation(() => <svg />)
 
-describe('wrapper component should work', () => {
+describe('v4 icon detection should work', () => {
   it('should detect icon version with simple props', () => {
     expect(isV4({})).toBe(false)
 
@@ -29,5 +30,84 @@ describe('wrapper component should work', () => {
     expect(isV4({ icon: 'github' })).toBe(true)
 
     expect(isV4({ icon: ['fab', 'github'] })).toBe(false)
+  })
+
+  it('should handle some complex conditions (not all covered)', () => {
+    expect(isV4({ v5: true, tag: 'span' })).toBe(false)
+
+    expect(isV4({ v5: true, fas: true })).toBe(false)
+    expect(isV4({ v5: true, far: true })).toBe(false)
+    expect(isV4({ v5: true, fal: true })).toBe(false)
+    expect(isV4({ v5: true, fab: true })).toBe(false)
+
+    expect(isV4({ tag: 'span', icon: 'github'  })).toBe(true)
+
+    expect(isV4({ fas: true, icon: 'github'  })).toBe(false)
+    expect(isV4({ far: true, icon: 'github'  })).toBe(false)
+    expect(isV4({ fal: true, icon: 'github'  })).toBe(false)
+    expect(isV4({ fab: true, icon: 'github'  })).toBe(false)
+  })
+})
+
+describe('wrapper should work', () => {
+  it('should give correct display name', () => {
+    expect(Icon.displayName).toBe('ReactFontawesome')
+  })
+
+  it('should mount', () => {
+    mount(<Icon name="google" />)
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)).toBeDefined()
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)[0].icon).toEqual(['fab', 'google'])
+
+    mount(<Icon icon={['fab', 'google']} />)
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)).toBeDefined()
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)[0].icon).toEqual(['fab', 'google'])
+
+    mount(<Icon icon={['fas', 'google']} />)
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)).toBeDefined()
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)[0].icon).toEqual(['fas', 'google'])
+  })
+
+  it('should pass props', () => {
+    mount(<Icon
+      name="google"
+      mask="google"
+      className="foobar"
+      spin
+      pulse
+      border
+      fixedWidth
+      inverse
+      listItem
+      flip="horizontal"
+      size="2x"
+      pull="left"
+      rotation={90}
+      transform="none"
+      symbol={false}
+    />)
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)[0]).toEqual({
+      border: true,
+      className: 'foobar',
+      fixedWidth: true,
+      flip: 'horizontal',
+      icon: ['fab', 'google'],
+      inverse: true,
+      listItem: true,
+      mask: ['fab', 'google'],
+      pull: 'left',
+      pulse: true,
+      rotation: 90,
+      size: '2x',
+      spin: true,
+      symbol: false,
+      transform: 'none',
+    })
+  })
+
+  it('should pass rotate as rotation', () => {
+    mount(<Icon name="google" rotate={90} />)
+
+    expect(last((FontAwesomeIcon as jest.Mock).mock.calls)[0].rotation).toEqual(90)
   })
 })
